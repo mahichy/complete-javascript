7fff83f28176932dea65e45a9ec62d562dc576e6
mahichy <mohiuddinoshim@yahoo.com>
2023-11-06 22:59:56 +0600

    challange 2 done

--- challange/script.js
+++ challange/script.js
@@ -4,15 +4,41 @@
 // let massJohn = 92;
 // let hightJohn = 1.95;
 
-let massMark = 95;
-let hightMark = 1.88;
-let massJohn = 85;
-let hightJohn = 1.76;
+// challange 1
+// let massMark = 95;
+// let hightMark = 1.88;
+// let massJohn = 85;
+// let hightJohn = 1.76;
 
-let BMIMark = massMark / hightMark ** 2;
-let BMIJohn = massJohn / (hightJohn * hightJohn);
-let largerBmi = BMIMark > BMIJohn;
-console.log(largerBmi);
-let total = BMIJohn + BMIMark;
-let average = (BMIMark + BMIJohn) / 2;
-console.log(BMIMark, BMIJohn, total, average);
+// let BMIMark = massMark / hightMark ** 2;
+// let BMIJohn = massJohn / (hightJohn * hightJohn);
+// let largerBmi = BMIMark > BMIJohn;
+// console.log(largerBmi);
+// let total = BMIJohn + BMIMark;
+// let average = (BMIMark + BMIJohn) / 2;
+// console.log(BMIMark, BMIJohn, total, average);
+
+// challange 2
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// if (BMIMark > BMIJohn) {
+//   console.log("Mark's BMI is higher than John's!");
+// } else {
+//   console.log("John's BMI is higher than Mark's!");
+// }
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+} else {
+  console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+}
+
+// "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!"
