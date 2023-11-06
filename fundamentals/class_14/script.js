4f98a1096f99cf8f7a7531631efe2269b6e67fc6
mahichy <mohiuddinoshim@yahoo.com>
2023-11-06 17:55:50 +0600

    Operator Precedence

---
+++ fundamentals/class_14/script.js
@@ -0,0 +1,24 @@
+let now = 2023;
+const ageJonas = now - 1982;
+const ageSarah = now - 1976;
+console.log(ageJonas);
+console.log(ageSarah);
+console.log(now - 1982 > now - 1976);
+console.log(now - 1982 >= now - 1976);
+console.log(now - 1982 < now - 1976);
+console.log(now - 1982 <= now - 1976);
+console.log(30 > 32);
+console.log(32 >= 32);
+
+let a, b;
+a = 10;
+b = 10;
+console.log(a, b);
+a = b = 25 - 10 - 5;
+console.log(a, b);
+a = 10;
+b = 10;
+console.log(a * b);
+
+a = b = (10 * 5) / 2;
+console.log(a, b);
